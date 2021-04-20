6338f1cb3323aed92b3d0451ee029aa1a380ed86
Mark <mdargie97@gmail.com>
2021-04-20 14:06:33 +0100

    Environment & VR Rig

---
+++ ConnectMinds/Assets/Scripts/VRRig.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class VRMap
+{
+    public Transform vrTarget;
+    public Transform rigTarget;
+    public Vector3 trackingPosOffset;
+    public Vector3 trackingRotOffset;
+
+    public void Map()
+    {
+        rigTarget.position = vrTarget.TransformPoint(trackingPosOffset);
+        rigTarget.rotation = vrTarget.rotation * Quaternion.Euler(trackingRotOffset);
+    }
+}
+
+public class VRRig : MonoBehaviour
+{
+
+    public VRMap head;
+    public VRMap leftHand;
+    public VRMap rightHand;
+
+
+    public Transform headContraint;
+    public Transform PositionSet;
+    public Vector3 headBodyOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        headBodyOffset = transform.position - headContraint.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        transform.position = PositionSet.position + headBodyOffset;
+        //transform.position = headContraint.position + headBodyOffset;
+        //transform.forward = Vector3.ProjectOnPlane(headContraint.up, Vector3.up).normalized;
+
+        head.Map();
+        //leftHand.Map();
+        //rightHand.Map();
+
+    }
+}
