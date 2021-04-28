6d224af96ffbdafd2ead691f2365766d94cf329c
Mark <mdargie97@gmail.com>
2021-04-28 20:20:25 +0100

    Cinematic Camera + Lighting Changes

---
+++ ConnectMinds/Assets/Scripts/CameraForwardMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraForwardMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 1f;
+    public Camera camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera.stereoTargetEye = StereoTargetEyeMask.None;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+    }
+
+    //private void FixedUpdate()
+    //{
+    //    transform.Translate(Vector3.forward * moveSpeed * Time.fixedDeltaTime);
+    //}
+}
